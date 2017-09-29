672c2643172e31a2b3c84ec80b87ca8046bc82d3
Randy <wi15b038@technikum-wien.at>
2017-09-29 11:45:46 +0200

    KonfliktRandy

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleApp2
             //Das ist der RandyBranchy
             //das is der khalli wally
 
-            //Test Test Test
+            //Test Test Test konflikt2
         }
     }
 }
